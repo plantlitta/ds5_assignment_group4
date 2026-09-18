6944c424de0e81a5d213875571c49a8164bb258b
plantlitta <577359@student.fontys.nl>
2026-09-18 13:30:50 +0200

    four functions that need to be made

--- ds4_litta.py
+++ ds4_litta.py
@@ -1,22 +1,24 @@
 
 file_path = input("Enter the path to the CSV file: ")
 records = []
-with open(file_path, 'r') as file:
+
+with open(file_path, 'r') as file: # 1. function to open file and make it a dictionary with lists
     csv_reader = csv.DictReader(file)
     for row in csv_reader:
         records.append(row)
 
-total = sum(float(record['Grade']) for record in records)
+total = sum(float(record['Grade']) for record in records) # 2. calculating total function
 average = total / len(records)
 
 print(f"Average Grade: {average}")
 print("--------------------")
 
-filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
+filtered_records = [record for record in records if float(record['Grade']) >= 80.0] # 3. filtering function
 
 print("Student Report")
 print("--------------")
-for record in filtered_records:
+for record in filtered_records: # 4. displaying results function
     print(f"Name: {record['Name']}")
     print(f"Grade: {record['Grade']}")
     print("--------------------")
+
