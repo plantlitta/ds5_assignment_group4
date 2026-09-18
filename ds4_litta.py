e54a832583574c3bf9048ac5b6480cdbbce36197
Anastasia Dmitrieva <574874@student.fontys.nl>
2026-09-18 14:16:30 +0200

    broke and fixed littas work

--- ds4_litta.py
+++ ds4_litta.py
@@ -9,32 +9,28 @@ with open(file_path, 'r') as file: # 1. function to open file and make it a dict
 
 
 #
-def import_data()
-
-
-
-
-
-
+def import_data():
+    file_path = input("Enter the path to the CSV file: ")
+    records = []
+    with open(file_path, 'r') as file: # 1. function to open file and make it a dictionary with lists
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            records.append(row)
+    return records 
 
 
 #
 
 
-#total = calc_tot(records) # 2. calculating total function
-#average = total / len(records)
+# total = sum(float(record['Grade']) for record in records) # 2. calculating total function
+# average = total / len(records)
 
-#
+# 
+
 def calc_tot(n): #as input has a column
     total = sum(float(record['Grade']) for record in n)
-    
-
 
     return total
-
-total = calc_tot(records) # 2. calculating total function
-average = total / len(records)
-
 #
 
 print(f"Average Grade: {average}")
@@ -43,9 +39,9 @@ print("--------------------")
 filtered_records = [record for record in records if float(record['Grade']) >= 80.0] # 3. filtering function
 
 #
-
-
-
+def filter_record(record):
+    filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
+    return(filtered_records)
 
 
 
@@ -56,12 +52,14 @@ filtered_records = [record for record in records if float(record['Grade']) >= 80
 
 print("Student Report")
 print("--------------")
-'''for record in filtered_records: # 4. displaying results function
+for record in filtered_records: # 4. displaying results function
     print(f"Name: {record['Name']}")
     print(f"Grade: {record['Grade']}")
-    print("--------------------")'''
+    print("--------------------")
 
 #
+
+
 
 def filter_list(b):
     for record in b: # 4. displaying results function
@@ -71,6 +69,9 @@ def filter_list(b):
     return None
 
 filter_list(filtered_records)
+
+
+
 
 
 #
