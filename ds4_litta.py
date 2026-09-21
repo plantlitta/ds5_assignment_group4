cd0ad5e34c0fea29c5289240e7d556d18d4d8b88
plantlitta <577359@student.fontys.nl>
2026-09-21 11:14:33 +0200

    adding docstrings to each function

--- ds4_litta.py
+++ ds4_litta.py
@@ -1,6 +1,6 @@
 
 
-def import_data():
+def import_data(): # done by Tacy # 1. function to open file and make it a dictionary with lists
     file_path = input("Enter the path to the CSV file: ")
     records = [] 
     with open(file_path, 'r') as file: 
@@ -10,23 +10,25 @@ def import_data():
     return records 
  
 
-def calc_tot(n):
+def calc_tot(n): # done by Litta # 2. calculating total function
     total = sum(float(record['Grade']) for record in n)
 
     return total
 
 
-def filter_record(record):
+def filter_record(record): #done by Tacy # 3. filtering function
     filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
     return(filtered_records)
 
 
-def filter_list(b):
+def filter_list(b): # done by Litta # 4. displaying results function
     for record in b: 
     print(f"Name: {record['Name']}")
     print(f"Grade: {record['Grade']}")
     print("--------------------")
     return None
+
+# calling all functions done by Litta
 
 records = import_data()
 filtered_records = filter_record(records)
